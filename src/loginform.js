bec76cf268210795f43447f7580449d0f4d17cc7
taratimmerman <timmermantara@gmail.com>
2021-01-05 17:36:12 -0700

    login page

---
+++ src/loginform.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import Login from './login'
+import './loginForm.css'
+
+const LoginForm = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false)
+
+    function submitForm() {
+        setIsSubmitted(true);
+    }
+    return (
+        <>
+        <div className='form-container'>
+            <span className='close-btn'>x</span>
+            <div className='form-content-left'>
+                <img className='form-img' src='img/pintereach.png' alt='logo' />
+            </div>
+            <Login />
+            {/* THE ABOVE LINE WILL BE REMOVED ONCE THE BELOW LOGIC IS COMPLETE */}
+            {/* {!isSubmitted ? <Login submitForm={submitForm} /> : TO DO - INSERT CODE TO MOVE ON TO ARTICLES AFTER LOGIN} */}
+        </div>
+        </>
+    )
+}
+
+export default LoginForm;
