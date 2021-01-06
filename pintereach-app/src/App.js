6df93f204b3d38d44faa1f5a74c5a5969ac8a4b1
JRIGS <raczkowskijonathan@gmail.com>
2021-01-06 15:00:16 -0500

    AxiosWithAuth and Router added

--- pintereach-app/src/App.js
+++ pintereach-app/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import SignUp from './components/SignUp'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+      <Route path="/sign-up" component={SignUp} />
+      <Route path="/" component={Login} />
+      </Switch>
     </div>
   );
 }
