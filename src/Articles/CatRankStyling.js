95232fb3334a18564893d3949c09a97035cb27a1
taratimmerman <timmermantara@gmail.com>
2021-01-08 08:43:44 -0700

    added form to change category and importance on saved articles

---
+++ src/Articles/CatRankStyling.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const FormDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CatRankLabel = styled.label`
+    color: #000;
+    padding: 10px;
+`;
+
+export const SelectCategory = styled.select`
+  padding-left: 10px;
+  outline: none;
+  border-radius: 2px;
+  height: 40px;
+  width: 150px;
+  margin: 5px;
+`;
+
+export const SelectImportance = styled.select`
+  padding-left: 10px;
+  outline: none;
+  border-radius: 2px;
+  height: 40px;
+  width: 50px;
+`;
--- src/Saved-Articles/SavedArticles.js
+++ src/Saved-Articles/SavedArticles.js
@@ -49,6 +49,7 @@ const SavedArticles = (props) => {
                         <option value='movies'>Movies</option>
                         <option value='music'>Music</option>
                         <option value='news'>News</option>
+                        <option value='politics'>Politics</option>
                         <option value='sports'>Sports</option>
                         <option value='technology'>Technology</option>
                         <option value='other'>Other</option>
